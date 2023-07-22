16ce569a09bb1f7a7d3d290d965a8b082f515f54
guga56 <gustavogregorio086@gmail.com>
2023-07-21 21:44:52 -0300

    inicializar o projeto c0om frontend

---
+++ DesenvolvimentoDados/DesenvolvimentoDados/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesenvolvimentoDados.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+        
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+
+        public IActionResult ListarPorNome()
+        {
+            return View();
+        }
+    }
+}
