f3549dc503b9c75b01da0a44247b278fef5bb416
guga56 <gustavogregorio086@gmail.com>
2023-07-21 21:53:06 -0300

    criar classe cliente no projeto

---
+++ DesenvolvimentoDados/DesenvolvimentoDados/Models/ClienteModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DesenvolvimentoDados.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Profissao { get; set; }
+    }
+}
