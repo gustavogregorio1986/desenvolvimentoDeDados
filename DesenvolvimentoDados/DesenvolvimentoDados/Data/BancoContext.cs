3a4a682020ac9da11a3cca443f70f1efbd4e2400
guga56 <gustavogregorio086@gmail.com>
2023-07-21 22:09:48 -0300

    construção do banco de dados com migrations

---
+++ DesenvolvimentoDados/DesenvolvimentoDados/Data/BancoContext.cs
@@ -0,0 +1,16 @@
+﻿using DesenvolvimentoDados.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesenvolvimentoDados.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<ClienteModel> Clientes { get; set; }
+    }
+}
---
+++ DesenvolvimentoDados/DesenvolvimentoDados/Migrations/20230722010543_Criartabelas.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesenvolvimentoDados.Migrations
+{
+    public partial class Criartabelas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Profissao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
